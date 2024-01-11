dd216844ddb3fc289e2a21c9d1102cd9af568e72
pedro <pedro@LAPTOP_PEDRO>
2024-01-10 21:32:17 -0600

    Agregar archivos de proyecto.

---
+++ HolaMundoVideojuegos/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace HolaMundoVideojuegos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F);
+            label1.Location = new Point(204, 63);
+            label1.Name = "label1";
+            label1.Size = new Size(390, 46);
+            label1.TabIndex = 0;
+            label1.Text = "Pedro Escobedo Straffon";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 20F);
+            label2.Location = new Point(339, 125);
+            label2.Name = "label2";
+            label2.Size = new Size(128, 46);
+            label2.TabIndex = 1;
+            label2.Text = "176110";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 20F);
+            label3.Location = new Point(204, 192);
+            label3.Name = "label3";
+            label3.Size = new Size(402, 46);
+            label3.TabIndex = 2;
+            label3.Text = "Graficacion y videojuegos";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
